0ea5a3b63e183fb7b9f0d6d06c884623ce5ab024
Aswathaman M <80340385+AswathAadi@users.noreply.github.com>
2024-10-20 16:24:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function SendEmail(){
+
+    var submitButton = document.getElementById("mail_button");
+            submitButton.value = "Submitted !";
+            submitButton.disabled = true;
+    var templateParams = {
+        from_name : document.getElementById("from_name").value,
+        from_email : document.getElementById("from_email").value,
+        from_mobile : document.getElementById("from_mobile").value,
+        from_message : document.getElementById("from_message_data").value
+      };
+
+      if (!templateParams.from_name || !templateParams.from_email || !templateParams.from_message) 
+      {
+        alert("Please fill in all required fields before submitting.");
+        return; // Don't proceed if any required field is empty
+      }
+    
+      emailjs.send('service_a5cnaeo', 'template_8pnt5pf', templateParams)
+        .then(function(response) 
+        {
+          console.log('SUCCESS!', response.status, response.text);
+        }, function(error) 
+        {
+          console.log('FAILED...', error);
+        });
+
+        // Clear the input fields after submission
+        document.getElementById("from_name").value = "";
+        document.getElementById("from_email").value = "";
+        document.getElementById("from_mobile").value = "";
+        document.getElementById("from_message_data").value = "";
+}
+
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var successMessage = document.createElement("p");
+    successMessage.textContent = "Form submitted successfully!";
+    document.body.appendChild(successMessage);
+});
